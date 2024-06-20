a0d7ad6900ce94e2a748d0b06178c3966fc80e27
nayye5154 <nayye5154@gmail.com>
2024-06-20 15:44:10 +0700

    isar test

---
+++ android/app/src/main/kotlin/com/example/isar_local_db_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.isar_local_db_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/user_model.dart
@@ -0,0 +1,12 @@
+import 'package:isar/isar.dart';
+
+part 'user_model.g.dart';
+
+@collection
+class User {
+  User(this.name, this.age);
+
+  Id id = Isar.autoIncrement;
+  String? name;
+  int? age;
+}
